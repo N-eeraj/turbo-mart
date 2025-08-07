4f4b6c598f2b66a7ffc260e79aad38281b94f0d0
N-eeraj <neeraj7rajeena@gmail.com>
2025-08-07 23:41:59 +0530

    Minor logger update

--- apps/catalogue-api/src/logger.ts
+++ apps/catalogue-api/src/logger.ts
@@ -3,7 +3,17 @@ import morgan from "morgan"
 
 import { withStoragePath } from "#utils/pathUtils"
 
-const logStream = fs.createWriteStream(withStoragePath("logs/log.txt"), { flags: "a" });
+const [
+  date,
+] = new Date()
+  .toISOString()
+  .split("T")
+
+const logStream = fs.createWriteStream(
+  withStoragePath(`logs/${date}.txt`), {
+    flags: "a",
+  }
+)
 const logFormat = "[:date[iso]] :method :url status-code::status :response-time ms"
 const logger = morgan(logFormat, {
   stream: logStream,
