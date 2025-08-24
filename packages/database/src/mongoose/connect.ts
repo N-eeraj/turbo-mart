c2f72bd64fe001b130b2080397c9ccc55a3ae976
N-eeraj <neeraj7rajeena@gmail.com>
2025-08-24 14:23:58 +0530

    moved mongo db connect to package

--- packages/database/src/mongoose/connect.ts
+++ packages/database/src/mongoose/connect.ts
@@ -1,18 +1,20 @@
 import mongoose from "mongoose"
 
+// mongo db uri configuration
+export const MONGODB_URI = process.env.MONGODB_URI
+
 /**
  * Establishes a connection to the mongo database.
  *
- * @param mongodbUri - The MongoDB connection URI
  * @param callback - An optional function to be executed once the connection is successfully established.
  */
-async function connect(mongodbUri: string, callback?: (connection: mongoose.Connection) => unknown) {
+async function connect(callback?: (connection: mongoose.Connection) => unknown) {
   try {
-    if (!mongodbUri) {
-      throw new Error("mongodbUri is not defined.")
+    if (!MONGODB_URI) {
+      throw new Error("MONGODB_URI environment variable is not defined.")
     }
     console.log("Trying to connect to database")
-    const { connection } = await mongoose.connect(mongodbUri)
+    const { connection } = await mongoose.connect(MONGODB_URI)
     console.log(`Database Connected: ${connection.host}`)
     await callback?.(connection)
   } catch (error) {
