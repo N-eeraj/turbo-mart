dc84a8f299fa6d37bd472b3b3e55452ebcd1d637
N-eeraj <neeraj7rajeena@gmail.com>
2025-09-14 14:32:43 +0530

    Open API Docs update

--- apps/admin-api/src/config/openApi/schemas.ts
+++ apps/admin-api/src/config/openApi/schemas.ts
@@ -1,6 +1,13 @@
 import type mongoose from "mongoose"
 
 import {
+  Roles,
+  Permissions,
+} from "@app/database/mongoose/models/Admin/User.ts"
+
+import NotificationSchema from "#docs/schemas/notification" with { type: "json" }
+import ProfileSchema from "#docs/schemas/profile" with { type: "json" }
+import {
   loginJSONSchema,
 } from "#schemas/auth"
 import {
@@ -27,8 +34,36 @@ const SortOrderEnum = {
   ] satisfies Array<mongoose.SortOrder>,
 }
 
+const RolesEnum = {
+  type: "number",
+  description: "Defines user roles and their access levels.\n- `0` - `SUPER_ADMIN`: Highest level admin with full privileges.\n- `1` - `ADMIN`: Standard admin with permission-based access.",
+  enum: [
+    Roles.SUPER_ADMIN,
+    Roles.ADMIN,
+  ] satisfies Array<Roles>,
+}
+
+const PermissionsEnum = {
+  type: "number",
+  description: "Defines role-based permissions within the system.\n- `0` - `RETAILER_MANAGER`: Permission to manage retailers.\n- `1` - `CATALOGUE_MANAGER`: Permission to manage product catalogues.\n- `2` - `DELIVERY_PERSON_MANAGER`: Permission to manage delivery personnel.\n- `3` - `FINANCE_MANAGER`: Permission to manage financial operations.\n- `4` - `DATA_ANALYST`: Permission to analyze data and generate reports.",
+  enum: [
+    Permissions.CATALOGUE_MANAGER,
+    Permissions.DATA_ANALYST,
+    Permissions.DELIVERY_PERSON_MANAGER,
+    Permissions.FINANCE_MANAGER,
+    Permissions.RETAILER_MANAGER,
+  ] satisfies Array<Permissions>,
+}
+
+const ResponseSchemas = {
+  ProfileSchema,
+  NotificationSchema,
+}
+
 const UtilitySchemas = {
   SortOrderEnum,
+  RolesEnum,
+  PermissionsEnum,
 }
 
 const schemas = {
@@ -38,7 +73,8 @@ const schemas = {
   ProfilePictureSchema: profilePictureJSONSchema,
   NotificationReadStatusSchema: notificationReadStatusJSONSchema,
   NotificationReadStatusBulkSchema: notificationReadStatusBulkJSONSchema,
-  ...UtilitySchemas
+  ...ResponseSchemas,
+  ...UtilitySchemas,
 }
 
 export default schemas
