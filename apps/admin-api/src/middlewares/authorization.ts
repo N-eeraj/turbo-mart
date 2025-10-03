ecba8dcbdd5b5aa45cba56a8fee09406ea5f1548
N-eeraj <neeraj7rajeena@gmail.com>
2025-10-03 14:06:42 +0530

    added permission based authorization middleware

--- apps/admin-api/src/middlewares/authorization.ts
+++ apps/admin-api/src/middlewares/authorization.ts
@@ -6,7 +6,8 @@ import {
 
 import {
   Roles,
-} from "@app/database/mongoose/models/Admin/User.js"
+  Permissions,
+} from "@app/database/mongoose/models/Admin/User.ts"
 
 import {
   sendResponse,
@@ -20,16 +21,18 @@ import {
  * 
  * @param req - The Express request object.
  * @param res - The Express response object.
- * @param next - The Express next middleware function. 
+ * @param next - The Express next middleware function.
  */
 export async function superAdminAuthorizationMiddleware(req: Request, res: Response, next: NextFunction) {
   try {
+    // throw unauthorized error if not super admin
     if (req.user.role !== Roles.SUPER_ADMIN) {
       throw {
         status: 403,
         message: "Super admin access required",
       }
     }
+
     next()
   } catch (error) {
     const {
@@ -37,7 +40,46 @@ export async function superAdminAuthorizationMiddleware(req: Request, res: Respo
       message,
       errors,
     } = formatError(error)
-    
+
     sendResponse(res, false, { status, message }, errors)
+  }
+}
+
+/**
+ * A factory function for the middleware to accept the required permissions.
+ * 
+ * @param requiredPermissions - The admin user permission required.
+ * 
+ * @returns the middleware that handles the authorization.
+ * 
+ * If user doesn't have the required permission or is not super admin,
+ * it'll send an error response with 403 status code.
+ */
+export function permissionAuthorizationMiddleware(...requiredPermissions: Array<Permissions>): Function {
+  return async function (req: Request, res: Response, next: NextFunction) {
+    try {
+      const hasPermission = (
+        req.user.role === Roles.SUPER_ADMIN ||
+        requiredPermissions.some(requiredPermission => req.user.permissions?.includes(requiredPermission))
+      )
+
+      // throw unauthorized error user is lacking required permission and isn't super admin
+      if (!hasPermission) {
+        throw {
+          status: 403,
+          message: "Unauthorized user",
+        }
+      }
+
+      next()
+    } catch (error) {
+      const {
+        status,
+        message,
+        errors,
+      } = formatError(error)
+  
+      sendResponse(res, false, { status, message }, errors)
+    }
   }
 }
