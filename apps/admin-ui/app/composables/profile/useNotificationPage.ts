93e0806860841a42f7b0011d59da5938af8384af
N-eeraj <neeraj7rajeena@gmail.com>
2025-11-23 21:02:10 +0530

    handle scroll pagination

--- apps/admin-ui/app/composables/profile/useNotificationPage.ts
+++ apps/admin-ui/app/composables/profile/useNotificationPage.ts
@@ -9,14 +9,14 @@ const NOTIFICATION_TYPE = [
   },
   {
     label: "Unread",
-    value: true,
+    value: false,
   },
   {
     label: "Read",
-    value: false,
+    value: true,
   },
 ] as const
-  
+
 const NOTIFICATION_QUERY_LIMIT = 12 as const
 
 export default function useNotificationPage() {
@@ -25,10 +25,12 @@ export default function useNotificationPage() {
 
   const notificationTypeIndex = ref(0)
   const notifications = ref<Array<Notification>>([])
+  const endOfList = ref(false)
 
   const {
     data: notificationsData,
     pending: isLoadingNotifications,
+    refresh: fetchNotifications,
   } = useLazyAsyncData(() => useApi("/profile/notifications", {
     query: {
       skip: notifications.value.length,
@@ -47,13 +49,15 @@ export default function useNotificationPage() {
   watch(() => notificationsData.value, (notification) => {
     if (notification) {
       notifications.value.push(...notification)
-      // endOfList.value = notification.length < NOTIFICATION_QUERY_LIMIT
+      endOfList.value = notification.length < NOTIFICATION_QUERY_LIMIT
     }
   })
 
   const handleNotificationTypeChange = (value: string | number) => {
     const type = NOTIFICATION_TYPE[Number(value)]?.label.toLowerCase() ?? ""
     notifications.value = []
+    endOfList.value = false
+    resetInfiniteScroll()
     router.replace({
       ...route,
       query: {
@@ -63,12 +67,32 @@ export default function useNotificationPage() {
     })
   }
 
+  const notificationListElement = useTemplateRef<HTMLElement>("notification-list")
+  const {
+    reset: resetInfiniteScroll,
+  } = useInfiniteScroll(
+    notificationListElement,
+    () => {
+      fetchNotifications()
+    },
+    {
+      distance: 10,
+      canLoadMore: () => {
+        return !isLoadingNotifications.value && !endOfList.value
+      },
+    }
+  )
+
   onMounted(() => {
     notificationTypeIndex.value = Math.max(
       0,
       NOTIFICATION_TYPE
         .findIndex(({ label }) => label.toLocaleLowerCase() === route.query.type)
     )
+  })
+
+  onBeforeUnmount(() => {
+    resetInfiniteScroll()
   })
 
   return {
