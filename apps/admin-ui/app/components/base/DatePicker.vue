2d1e658226de996dd6331e9224df5cb0f1343ddb
N-eeraj <neeraj7rajeena@gmail.com>
2026-01-08 21:17:17 +0530

    fix(admin-ui): Add `BaseDatePicker` component

---
+++ apps/admin-ui/app/components/base/DatePicker.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import {
+  DateFormatter,
+  getLocalTimeZone,
+  today,
+  type DateValue,
+} from "@internationalized/date"
+import {
+  Calendar,
+} from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import {
+  cn,
+} from "@/lib/utils"
+
+const modelValue = defineModel<DateValue>()
+
+const defaultPlaceholder = today(getLocalTimeZone())
+const df = new DateFormatter("en-US", {
+  dateStyle: "long",
+})
+</script>
+
+<template>
+  <Popover>
+    <PopoverTrigger as-child>
+      <BaseButton
+        variant="outline"
+        :class="cn(
+          'w-[280px] justify-start text-left font-normal',
+          !modelValue && 'text-muted-foreground',
+        )">
+        <Icon
+          name="lucide:calendar"
+          class="mr-2" />
+        {{ modelValue ? df.format(modelValue.toDate(getLocalTimeZone())) : "Pick a date" }}
+      </BaseButton>
+    </PopoverTrigger>
+    <PopoverContent class="w-auto p-0">
+      <Calendar
+        v-model="modelValue"
+        :initial-focus="true"
+        :default-placeholder
+        layout="month-and-year" />
+    </PopoverContent>
+  </Popover>
+</template>
