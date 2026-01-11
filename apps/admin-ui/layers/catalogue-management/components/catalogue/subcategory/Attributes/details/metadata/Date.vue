7adaf0780454c3c7b110a054a54bb23d2b04a589
N-eeraj <neeraj7rajeena@gmail.com>
2026-01-11 20:42:18 +0530

    feat(admin-ui): Attribute Metadata UI

--- apps/admin-ui/layers/catalogue-management/components/catalogue/subcategory/Attributes/details/metadata/Date.vue
+++ apps/admin-ui/layers/catalogue-management/components/catalogue/subcategory/Attributes/details/metadata/Date.vue
@@ -13,9 +13,39 @@ const props = defineProps<Props>()
 
 const minDate = computed(() => props.metadata?.min && new Date(props.metadata.min).toDateString())
 const maxDate = computed(() => props.metadata?.max && new Date(props.metadata.max).toDateString())
+
+
+const metadataRows = computed(() => [
+  {
+    label: "Min Date",
+    value: minDate.value,
+  },
+  {
+    label: "Max Date",
+    value: maxDate.value,
+  },
+])
 </script>
 
 <template>
-  {{ minDate }}
-  {{ maxDate }}
+  <ul>
+    <li
+      v-for="({ label, value }, index) in metadataRows"
+      :key="index"
+      class="text-sm">
+      <span class="text-foreground/75">
+        {{ label }}:
+      </span>
+      <strong
+        v-if="value"
+        class="font-medium">
+        {{ value }}
+      </strong>
+      <span
+        v-else
+        class="text-foreground/75">
+        -
+      </span>
+    </li>
+  </ul>
 </template>
--- apps/admin-ui/layers/catalogue-management/components/catalogue/subcategory/Attributes/details/metadata/Number.vue
+++ apps/admin-ui/layers/catalogue-management/components/catalogue/subcategory/Attributes/details/metadata/Number.vue
@@ -5,17 +5,56 @@ import {
 import {
   AttributeObject,
 } from "@app/database/mongoose/models/Catalogue/Attributes"
+import {
+  cn,
+} from "@/lib/utils"
 
 interface Props{
   metadata?: AttributeObject<AttributeType.NUMBER>["metadata"]
 }
 const props = defineProps<Props>()
+
+const metadataRows = computed(() => [
+  {
+    label: "Base Value",
+    value: props.metadata?.base,
+  },
+  {
+    label: "Unit",
+    value: props.metadata?.unit,
+  },
+  {
+    label: "Min Value",
+    value: props.metadata?.min,
+  },
+  {
+    label: "Max Value",
+    value: props.metadata?.max,
+  },
+  {
+    label: "Template",
+    value: props.metadata?.template,
+  },
+])
 </script>
 
 <template>
-  {{ metadata?.base }}
-  {{ metadata?.max }}
-  {{ metadata?.min }}
-  {{ metadata?.template }}
-  {{ metadata?.unit }}
+  <table class="text-sm w-full border-collapse">
+    <tbody>
+      <tr
+        v-for="({ label, value }, index) in metadataRows"
+        :key="index">
+        <td class="text-foreground/75">
+          {{ label }}:
+        </td>
+        <td
+          :class="cn(
+            'font-medium',
+            !value && 'text-foreground/75'
+          )">
+          {{ value ?? "-" }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
