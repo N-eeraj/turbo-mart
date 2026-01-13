6001d052b25d92bead2f61de66eab3bce1350c02
N-eeraj <neeraj7rajeena@gmail.com>
2026-01-13 20:21:52 +0530

    feat(admin-ui): Text select option UI

--- apps/admin-ui/layers/catalogue-management/components/catalogue/subcategory/Attributes/Form/metadata/Select.vue
+++ apps/admin-ui/layers/catalogue-management/components/catalogue/subcategory/Attributes/Form/metadata/Select.vue
@@ -13,27 +13,49 @@ interface Props {
   field: "create" | "update"
   index: number
 }
-defineProps<Props>()
+const props = defineProps<Props>()
 
 const OPTIONS = [
   {
     label: "Text",
-    value: AttributeType.TEXT,
+    value: String(AttributeType.TEXT),
   },
   {
     label: "Number",
-    value: AttributeType.NUMBER,
+    value: String(AttributeType.NUMBER),
   },
 ] as const
+
+const optionsType = ref<typeof OPTIONS[number]["value"]>(OPTIONS[0].value)
+
+const {
+  fields: optionFields,
+  push: optionPush,
+  remove: optionRemove,
+} = useFieldArray(`${props.field}[${props.index}].metadata.options`)
+
+function addOption() {
+  optionPush(
+    optionsType.value === String(AttributeType.TEXT)
+      ? ""
+      : {}
+  )
+}
+
+watch(() => optionsType.value, () => {
+  optionFields.value = []
+})
 </script>
 
 <template>
   <div class="grid md:grid-cols-2 gap-3">
     <FormFieldRadio
       :name="`${field}[${index}].metadata.type`"
+      v-model="optionsType"
       :options="OPTIONS"
       label="Option Type"
-      radio-group-class="flex gap-x-0">
+      radio-group-class="flex gap-x-0"
+      class="md:col-span-2">
       <template #item="{ value, label }">
         <Label class="p-3 has-aria-checked:bg-primary/10 border has-aria-checked:border-primary/75 cursor-pointer">
           <RadioGroupItem :value />
@@ -43,5 +65,46 @@ const OPTIONS = [
         </Label>
       </template>
     </FormFieldRadio>
+
+    <div class="grid grid-cols-[1fr_auto] items-center gap-2 md:col-span-2">
+      <small>
+        Options
+      </small>
+      <BaseButton
+        variant="outline"
+        type="button"
+        size="icon-sm"
+        class="size-8 border-primary/50"
+        @click="addOption">
+        <Icon
+          name="lucide:plus"
+          :size="12"
+          class="text-primary" />
+      </BaseButton>
+      <ul class="col-span-2 space-y-2">
+        <li
+          v-for="(option, optionIndex) in optionFields"
+          :key="option.key"
+          class="flex justify-between items-center gap-x-3 md:gap-x-4">
+          <div class="flex-1">
+            <FormFieldInput
+              v-if="optionsType === String(AttributeType.TEXT)"
+              :name="`${field}[${index}].metadata.options[${optionIndex}]`"
+              :placeholder="`Option ${optionIndex + 1}`" />
+          </div>
+          <BaseButton
+            variant="outline"
+            type="button"
+            size="icon-sm"
+            class="aspect-square border-destructive/50"
+            @click="optionRemove(optionIndex)">
+            <Icon
+              name="lucide:minus"
+              :size="12"
+              class="text-destructive" />
+          </BaseButton>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
