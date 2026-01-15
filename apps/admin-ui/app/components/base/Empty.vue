e43dbe81e001bc51ee37775dd86e6adfcf7989c9
N-eeraj <neeraj7rajeena@gmail.com>
2026-01-15 21:38:38 +0530

    feat(admin-ui): Update subcategory attributes empty state

---
+++ apps/admin-ui/app/components/base/Empty.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty"
+
+interface Props {
+  title?: string
+  description?: string
+  mediaVariant?: "default" | "icon" | null | undefined
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <Empty>
+    <EmptyHeader>
+      <EmptyMedia :variant="mediaVariant">
+        <slot name="media" />
+      </EmptyMedia>
+      <EmptyTitle>
+        <slot
+          name="title"
+          :title>
+          {{ title }}
+        </slot>
+      </EmptyTitle>
+      <EmptyDescription>
+        <slot
+          name="description"
+          :description>
+          {{ description }}
+        </slot>
+      </EmptyDescription>
+    </EmptyHeader>
+    <EmptyContent>
+      <slot name="content" />
+    </EmptyContent>
+    <slot />
+  </Empty>
+</template>
--- apps/admin-ui/layers/catalogue-management/components/catalogue/subcategory/Attributes/index.vue
+++ apps/admin-ui/layers/catalogue-management/components/catalogue/subcategory/Attributes/index.vue
@@ -15,33 +15,51 @@ const props = defineProps<Props>()
 
 <template>
   <section>
-    <div class="flex items-baseline gap-x-1">
-      <h2 class="text-lg md:text-xl">
-        Attributes
-      </h2>
+    <template v-if="attributes?.length">
+      <div class="flex items-baseline gap-x-1">
+        <h2 class="text-lg md:text-xl">
+          Attributes
+        </h2>
+        <NuxtLink :to="`/catalogue/subcategories/${slug}/attributes`">
+          <BaseButton
+            variant="ghost"
+            size="icon-sm">
+            <Icon name="lucide-edit" />
+          </BaseButton>
+        </NuxtLink>
+      </div>
+
+      <MasonryWall
+        :items="attributes"
+        :column-width="280"
+        :gap="12"
+        class="mt-1">
+        <template #default="{ item }">
+          <CatalogueSubcategoryAttributesDetails v-bind="item" />
+        </template>
+      </MasonryWall>
+    </template>
+
+    <BaseEmpty
+      v-else
+      title="No Attributes"
+      description="There are no attributes defined for this subcategory.">
+      <template #media>
+        <Icon
+          name="lucide:sparkle" />
+      </template>
       <NuxtLink :to="`/catalogue/subcategories/${slug}/attributes`">
         <BaseButton
-          variant="ghost"
-          size="icon-sm">
-          <Icon name="lucide-edit" />
+          variant="outline"
+          class="hover:bg-primary/10 order-primary/50">
+          <span>
+            Create Attributes
+          </span>
+          <Icon
+            name="lucide:plus"
+            class="text-primary" />
         </BaseButton>
       </NuxtLink>
-    </div>
-
-    <MasonryWall
-      v-if="attributes?.length"
-      :items="attributes"
-      :column-width="280"
-      :gap="12"
-      class="mt-1">
-      <template #default="{ item }">
-        <CatalogueSubcategoryAttributesDetails v-bind="item" />
-      </template>
-    </MasonryWall>
-    <p
-      v-else
-      class="md:mt-1 text-xs md:text-sm text-foreground/60">
-      No Attributes
-    </p>
+    </BaseEmpty>
   </section>
 </template>
