4c010f504a37b6432f04d525b7bee560d796a2a1
N-eeraj <neeraj7rajeena@gmail.com>
2026-02-15 22:20:27 +0530

    refactor(admin-ui): split base & variant attributes to separate components * add info tooltip

---
+++ apps/admin-ui/layers/catalogue-management/components/catalogue/product/Form/Attributes/VariantAttributes.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {
+  AttributeType,
+} from "@app/database/mongoose/enums/catalogue/attribute"
+import {
+  type AttributeObject,
+} from "@app/database/mongoose/models/Catalogue/Attributes"
+
+interface Props {
+  attributes: Array<Omit<AttributeObject<AttributeType>, "variant">>
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <section class="space-y-1">
+    <div class="space-x-0.5">
+      <strong class="text-sm">
+        Variant Attributes
+      </strong>
+      <BaseTooltip>
+        <Icon
+          name="lucide:info"
+          :size="12" />
+        <template #tooltip>
+          <p class="max-w-md">
+            Defines the characteristics that create product variations.
+            Products can use multiple attributes, combined to form unique variants.
+          </p>
+        </template>
+      </BaseTooltip>
+    </div>
+
+    <ul class="space-y-2">
+      <li
+        v-for="attribute in attributes"
+        :key="attribute.id as unknown as string">
+        {{ attribute }}
+      </li>
+    </ul>
+  </section>
+</template>
---
+++ apps/admin-ui/layers/catalogue-management/components/catalogue/product/Form/Attributes/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Emits {
+  submit: [unknown]
+}
+const emit = defineEmits<Emits>()
+
+const {
+  isLoadingProductAttributes,
+  isLoadingSubcategoryAttributes,
+  subcategoryAttributesMap,
+} = useProductFormAttributes(emit)
+</script>
+
+<template>
+  <BaseLinearProgress v-if="isLoadingProductAttributes || isLoadingSubcategoryAttributes" />
+  <form
+    v-else
+    class="space-y-3">
+    <CatalogueProductFormAttributesBaseAttributes :attributes="subcategoryAttributesMap.base" />
+    <CatalogueProductFormAttributesVariantAttributes :attributes="subcategoryAttributesMap.variant" />
+  </form>
+</template>
