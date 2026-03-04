bc561e183bd5d6e5ee3cb4997121170bbb439ab0
N-eeraj <neeraj7rajeena@gmail.com>
2026-03-04 13:17:24 +0530

    feat [admin/ui/catalogue]: Add boolean metadata form

---
+++ apps/admin-ui/layers/catalogue-management/components/catalogue/subcategory/Attributes/Form/metadata/Boolean.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Props {
+  field: "create" | "update"
+  index: number
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="grid md:grid-cols-2 gap-3">
+    <FormFieldInput
+      :name="`${field}[${index}].metadata.trueValue`"
+      label="True Text"
+      description="Text displayed for true value"
+      clearable />
+    <FormFieldInput
+      :name="`${field}[${index}].metadata.falseValue`"
+      label="False Text"
+      description="Text displayed for false value"
+      clearable />
+  </div>
+</template>
